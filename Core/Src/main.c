291878a975ca9123cb82181d002cb8ca7d3a2069
lorinczstefanmihai <lorincz.stefanmihai@yahoo.ro>
2024-02-08 14:36:53 +0200

    updata main and msp also

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,15 +13,26 @@ void UART_Init();
 void Error_handler();
 
 UART_HandleTypeDef console_output;
+UART_HandleTypeDef uart_gps;
+
+
+char data;
+
+char start_console_message[30] = "---- STM32 RUNNNING ----";
+char loading_console_message[40] = "GPS MODULE IS SEARCHING SIGNAL \n\r";
 
 int main()
 {
 	HAL_Init();
 	UART_Init();
 
+	HAL_UART_Transmit(&console_output,(uint8_t*)&start_console_message, 24, HAL_UART_TIMEOUT_VALUE);
+
 
 	while(1)
 	{
+		if(HAL_UART_Receive(&uart_gps,(uint8_t*)&data, 1, 100) == HAL_OK)
+			HAL_UART_Transmit(&console_output, (uint8_t*)&data, 1, HAL_UART_TIMEOUT_VALUE);
 	}
 
 }
@@ -36,6 +47,18 @@ void UART_Init()
 	console_output.Init.WordLength = UART_WORDLENGTH_8B;
 	console_output.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	if(HAL_UART_Init(&console_output) != HAL_OK)
+	{
+		Error_handler();
+	}
+
+	uart_gps.Instance = USART1;
+	uart_gps.Init.BaudRate = 9600;
+	uart_gps.Init.Mode = UART_MODE_TX_RX;
+	uart_gps.Init.Parity = UART_PARITY_NONE;
+	uart_gps.Init.StopBits = UART_STOPBITS_1;
+	uart_gps.Init.WordLength = UART_WORDLENGTH_8B;
+	uart_gps.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	if(HAL_UART_Init(&uart_gps) != HAL_OK)
 	{
 		Error_handler();
 	}
--- Core/Src/msp.c
+++ Core/Src/msp.c
@@ -21,9 +21,17 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart)
 {
   GPIO_InitTypeDef gpio_uart;
 
+  /********************enable clocks********************/
+
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  __HAL_RCC_USART1_CLK_ENABLE();
   __HAL_RCC_USART2_CLK_ENABLE();
 
+  /********************pin muxing***********************/
+
+  /* UART TX CONSOLE */
   gpio_uart.Pin = GPIO_PIN_2;
   gpio_uart.Mode = GPIO_MODE_AF_PP;
   gpio_uart.Alternate = GPIO_AF1_USART2;
@@ -31,5 +39,16 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart)
   gpio_uart.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &gpio_uart);
 
+  /* UART TX GPS */
+  gpio_uart.Pin = GPIO_PIN_4;
+  gpio_uart.Mode = GPIO_MODE_AF_PP;
+  gpio_uart.Alternate = GPIO_AF1_USART1;
+  gpio_uart.Pull = GPIO_PULLUP;
+  gpio_uart.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOC, &gpio_uart);
+
+  /* UART RX GPS */
+  gpio_uart.Pin = GPIO_PIN_5;
+  HAL_GPIO_Init(GPIOC, &gpio_uart);
 
 }
